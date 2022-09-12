0218895c78839d1013924fba127de534a0f27748
Priyanka-koli <kolipriyu11@gmail.com>
2022-09-12 13:13:06 +0530

    add dynamic table netry fields

--- src/components/pages/Expenses/AllExpenses.js
+++ src/components/pages/Expenses/AllExpenses.js
@@ -9,14 +9,20 @@ import {
   TableRow,
   Paper,
   tableCellClasses,
+  TextField,
+  Box,
+  Button,
+  Typography,
 } from "@mui/material";
-
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { getExpenseFromApi, deleteExpenseById } from "../../../service/api";
 import { useNavigate } from "react-router-dom";
 import TotalExpense from "./TotalExpenses";
 import DialogExpenses from "./DialogExpenses";
+import { addExpenseToApi } from "../../../service/api";
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: "#063970",
@@ -37,10 +43,24 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
+const INITAIL_VALUES = {
+  id: 0,
+  expense_title: "",
+  expense_amount: "",
+  expense_date: "",
+};
+
 export default function AllExpenses() {
   const [expenses, setExpenses] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
-  const [expenseDetails , setExpenseDetails] = useState([]);
+  const [expenseDetails, setExpenseDetails] = useState([]);
+  const [newExpenseEntry, setNewExpenseEntry] = useState(expenses);
+
+  //for validation
+  const [isTitleEntered, setIsTitleEntered] = useState(true);
+  const [isAmountEntered, setIsAmountEntered] = useState(true);
+  const [isDateEntered, setIsdateEntered] = useState(true);
+
   const Navigate = useNavigate();
   /* calling getExpenseFromApi */
   const getAllExpense = async () => {
@@ -61,74 +81,148 @@ export default function AllExpenses() {
     Navigate(`/edit-expenses/${id}`);
   };
 
-  const openDailogHandler = (expenseData) =>{
+  const openDailogHandler = (expenseData) => {
     setOpenDialog(true);
     setExpenseDetails(expenseData);
-  }
+  };
 
   //calculating total of expenses
   const totalAmount = expenses.reduce((acc, amount) => {
     return (acc += Number(amount["expense_amount"]));
   }, 0);
 
+  //function to handle table dynamic entry change
+  const onChangeHandler = (event) => {
+    if (event.target.name === "expense_title") {
+      setIsTitleEntered(true);
+    }
+    if (event.target.name === "expense_amount") {
+      setIsAmountEntered(true);
+    }
+    if (event.target.name === "expense_date") {
+      setIsdateEntered(true);
+    }
+    setNewExpenseEntry({
+      ...newExpenseEntry,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  //function to handle table submit entry
+  const addHandler = async (event) => {
+    !newExpenseEntry.expense_title && setIsTitleEntered(false);
+    !newExpenseEntry.expense_amount && setIsAmountEntered(false);
+    !newExpenseEntry.expense_date && setIsdateEntered(false);
+
+    if (
+      !newExpenseEntry.expense_title ||
+      !newExpenseEntry.expense_amount ||
+      !newExpenseEntry.expense_date
+    ) {
+      return;
+    }
+    await addExpenseToApi(newExpenseEntry);
+    getAllExpense();
+  };
+
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table
-          sx={{ maxWidth: 900, margin: "5% auto" }}
-          aria-label="customized table"
-        >
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Id</StyledTableCell>
-              <StyledTableCell align="center">Title</StyledTableCell>
-              <StyledTableCell align="center">Amount($)</StyledTableCell>
-              <StyledTableCell align="center">Date</StyledTableCell>
-              <StyledTableCell align="center">Edit</StyledTableCell>
-              <StyledTableCell align="center">Delete</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {expenses.map((expense) => (
-              <StyledTableRow key={expense.name}>
-                <StyledTableCell component="th" scope="row">
-                  {expense.id}
-                </StyledTableCell>
-                <StyledTableCell
-                  align="center"
-                  onClick={() => openDailogHandler(expense)}
-                >
-                  {expense.expense_title}
+      <Box>
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ maxWidth: 900, margin: "5% auto" }}
+            aria-label="customized table"
+          >
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Id</StyledTableCell>
+                <StyledTableCell align="center">Title</StyledTableCell>
+                <StyledTableCell align="center">Amount($)</StyledTableCell>
+                <StyledTableCell align="center">Date</StyledTableCell>
+                <StyledTableCell align="center">Actions</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <StyledTableRow key="expense-entry">
+                <StyledTableCell scope="row">Entry</StyledTableCell>
+                <StyledTableCell align="center">
+                  <TextField
+                    type="text"
+                    onChange={onChangeHandler}
+                    name="expense_title"
+                    error={!isTitleEntered}
+                  ></TextField>
                 </StyledTableCell>
                 <StyledTableCell align="center">
-                  {expense.expense_amount}
+                  <TextField
+                    type="number"
+                    onChange={onChangeHandler}
+                    name="expense_amount"
+                    error={!isAmountEntered}
+                  ></TextField>
                 </StyledTableCell>
                 <StyledTableCell align="center">
-                  {expense.expense_date}
+                  <TextField
+                    type="date"
+                    onChange={onChangeHandler}
+                    name="expense_date"
+                    error={!isDateEntered}
+                  ></TextField>
                 </StyledTableCell>
                 <StyledTableCell align="center">
-                  <ModeEditIcon
-                    sx={{ fontsize: "Medium", color: "#06397" }}
-                    onClick={() => editExpenseHandler(expense.id)}
-                  />
-                </StyledTableCell>
-                <StyledTableCell align="center">
-                  <DeleteIcon
-                    sx={{ fontsize: "Medium", color: "#06397" }}
-                    onClick={() => deleteExpenseHandler(expense.id)}
-                  />
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{ color: "primary" }}
+                    onClick={addHandler}
+                  >
+                    <AddCircleIcon />
+                  </Button>
                 </StyledTableCell>
               </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      {<TotalExpense expenseTotal={totalAmount}></TotalExpense>}
-      <DialogExpenses
-        open={openDialog}
-        onClose={() => setOpenDialog(false)}
-        data = {expenseDetails}
-      ></DialogExpenses>
+              {expenses.map((expense) => (
+                <StyledTableRow key={expense.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {expense.id}
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="center"
+                    onClick={() => openDailogHandler(expense)}
+                  >
+                    {expense.expense_title}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {expense.expense_amount}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    {expense.expense_date}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    <ModeEditIcon
+                      sx={{ fontsize: "Medium", color: "#06397" }}
+                      onClick={() => editExpenseHandler(expense.id)}
+                    />
+                    <DeleteIcon
+                      sx={{
+                        fontsize: "Medium",
+                        color: "#06397",
+                        marginLeft: "2rem",
+                      }}
+                      onClick={() => deleteExpenseHandler(expense.id)}
+                    />
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {<TotalExpense expenseTotal={totalAmount}></TotalExpense>}
+        <DialogExpenses
+          open={openDialog}
+          onClose={() => setOpenDialog(false)}
+          data={expenseDetails}
+        ></DialogExpenses>
+      </Box>
     </>
   );
 }
