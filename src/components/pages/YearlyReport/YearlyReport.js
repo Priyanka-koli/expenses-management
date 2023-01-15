062ec7a4d467a8053c09be18555c5887f734d848
Priyanka-koli <kolipriyu11@gmail.com>
2023-01-15 23:54:25 +0530

    add functionality to yearlyreport component

--- src/components/pages/YearlyReport/YearlyReport.js
+++ src/components/pages/YearlyReport/YearlyReport.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
 
 const YearlyReport = () => {
-  return <div>Yearly report </div>;
+  const [enterdNumber, setEnteredNumber] = useState({});
+  const [rgbColor, setRgbColor] = useState({});
+  const monthsArr = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const rgbCalculator = (enterNumber, pos) => {
+    /* const red = Math.floor(enterNumber / (256 * 256));
+    const green = Math.floor(enterNumber / 256) % 256;
+    const blue = enterNumber % 256; */
+    const hexNum = `#${Number(enterNumber).toString(16).padEnd(2, 0)}`;
+    console.log(hexNum);
+    const hexNumber =
+      "#" +
+      hexNum
+        .slice(hexNum.startsWith("#") ? 1 : 0)
+        .split("")
+        .map((x) => x + x)
+        .join("");
+    console.log(hexNumber);
+    setRgbColor({
+      ...rgbColor,
+      [pos]: enterNumber < 9007 ? hexNumber : hexNum,
+    });
+  };
+
+  const fieldChangeHanlder = (e) => {
+    setEnteredNumber({ ...enterdNumber, [e.target.name]: e.target.value });
+    rgbCalculator(e.target.value, e.target.name);
+  };
+
+  return (
+    <div>
+      {monthsArr.map((ele, i) => (
+        <TextField
+          name={String(i)}
+          sx={{ backgroundColor: rgbColor[i] }}
+          value={enterdNumber[i]}
+          onChange={fieldChangeHanlder}
+        ></TextField>
+      ))}
+    </div>
+  );
 };
 
 export default YearlyReport;
